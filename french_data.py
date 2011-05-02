341dee5542d76118c1c41bc1549b229d810e4122
Ramesh Nidadavolu <ramesh.nid@gmail.com>
2011-05-02 08:13:10 +0530

    First Commit

---
+++ french_data.py
@@ -0,0 +1,11 @@
+
+from scipy import stats
+import scipy as sp
+import numpy as np
+import matplotlib as mpl
+from matplotlib import pyplot as plt
+from os import sys
+import random
+import math
+
+
